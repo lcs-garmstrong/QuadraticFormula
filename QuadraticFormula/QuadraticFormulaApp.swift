b4a5f21f79ca7275d50c33bc1cc66169a7f6e9c0
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-24 11:09:56 -0500

    Initial Commit

---
+++ QuadraticFormula/QuadraticFormulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticFormulaApp.swift
+//  QuadraticFormula
+//
+//  Created by Graeme Armstrong on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticFormulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
